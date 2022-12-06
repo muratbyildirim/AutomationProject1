d4613a9afd00e17ce9b96916802fd6b64d21b291
Murat Yıldırım <muratbyildirim@gmail.com>
2022-12-06 23:51:32 +0300

    more commit and push practice

---
+++ src/gitPractice2/CommitAndPush.java
@@ -0,0 +1,13 @@
+package gitPractice2;
+
+public class CommitAndPush {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("more practice");
+        }
+
+    }
+
+}
