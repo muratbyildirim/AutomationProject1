4e03f6c79ff278c5c32b09114a311fa73a1fdbb3
Murat Yıldırım <muratbyildirim@gmail.com>
2022-12-06 23:40:02 +0300

    initial commit

---
+++ src/gitHubPratice/Hello.java
@@ -0,0 +1,12 @@
+package gitHubPratice;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Commit and Push practice");
+
+    }
+
+}
